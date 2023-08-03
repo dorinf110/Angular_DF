93cdc74f15fe87ed1bd8bec775dd540d423d76c2
dorinf110 <113466719+dorinf110@users.noreply.github.com>
2023-08-03 21:14:15 +0300

    Faza 03.08

--- First Project/app/src/app/app.component.ts
+++ First Project/app/src/app/app.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   firstname:String="Tiberiu";
-  lastname:String="Vlad"
+  lastname:String="Vlad";
+  texth1:String="";
+
+  value:String="";
+
+  colorvalue:String="";
+  fontColor:String="";
+  
+  
+  OnClick(){
+    this.firstname="Vasile";
+    this.lastname="George";
+    this.texth1="Am adaugat text;"
+  }
+
+  Click2(){
+    this.value="Hello world!"
+  }
+
+  Click3(){
+    this.fontColor=this.colorvalue;
+  }
 ;}
